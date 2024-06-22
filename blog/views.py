c3d4dbd691a85cf6ace992a46fb669daf8bc7990
eyob <eyobmamo25@gmail.com>
2024-06-23 01:44:49 +0300

    use custem html site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+# because of template dose not exit error we change theis by the below
+# def post_list(request):
+#     return render(request,'blog/templates/blog/post_list.html',{})
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
