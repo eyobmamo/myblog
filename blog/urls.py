a52a9a30d4a59176b5026e1feb96e0d431aa36f5
eyob <eyobmamo25@gmail.com>
2024-06-23 18:16:57 +0300

    change to priv

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    
 ]
 
